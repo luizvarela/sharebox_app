2b9ff1bd42dbc6fe37f2b9e0da6def4c0a3f32b1
luiz <luiz.lvarela@gmail.com>
2012-09-02 10:35:14 -0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharebox::Application.config.secret_token = '808f984107b52e3f6f08a4b42fc357b01c0ea698c75af56549bc07614450ae703257167512c2d98f7b712f8e6d15ccd678157328f2a8e457858d33559f97027f'
---
+++ db/migrate/20120901101841_add_folder_id_to_assets.rb
@@ -0,0 +1,12 @@
+class AddFolderIdToAssets < ActiveRecord::Migration
+  def change
+    add_column :assets, :folder_id, :integer
+    add_index :assets, :folder_id
+  
+  end
+  
+  def self.down
+    remove_column :assets, :folder_id
+  end
+  
+end
